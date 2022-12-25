8d48ba68977ece58ad48752f0fffeea4572ed408
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2022-12-25 22:49:37 +0530

    Minor Fixes

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,15 +1,16 @@
+import { ReactNode } from 'react';
 import styles from './alert.module.css';
 import clsx from 'clsx';
 
 type AlertProps = {
-  children: ReactNode,
-  type: string,
+  children: ReactNode;
+  type: string;
 };
 
 export default function Alert({ children, type }: AlertProps) {
   return (
     <div
-      className={cn({
+      className={clsx({
         [styles.success]: type === 'success',
         [styles.error]: type === 'error',
       })}
