94df8793655af1fc93c7f703e606764c6b0fa51e
Piyush Aggarwal <web.piyushaggarwal@gmail.com>
2023-06-12 11:36:18 +0530

    chore: update README and comments

--- todo-list-kyle/src/db.ts
+++ todo-list-kyle/src/db.ts
@@ -1,13 +1,15 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '@prisma/client'
+
+// This makes sure the prisma client instance isn't created again and again while hot reload
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
+	prisma: PrismaClient | undefined
 }
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"],
-  })
+	globalForPrisma.prisma ??
+	new PrismaClient({
+		log: ['query'],
+	})
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
